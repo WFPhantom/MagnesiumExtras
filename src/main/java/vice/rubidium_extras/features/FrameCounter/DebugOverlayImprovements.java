6d4cb78e614ab3c75c2f48d0daa690e77f863a39
Veselin Plochev <89593443+WFPhantom@users.noreply.github.com>
2022-12-20 14:28:26 +0200

    this should maybe fix fps counter

--- src/main/java/vice/rubidium_extras/features/FrameCounter/DebugOverlayImprovements.java
+++ src/main/java/vice/rubidium_extras/features/FrameCounter/DebugOverlayImprovements.java
@@ -3,15 +3,19 @@ package vice.rubidium_extras.features.FrameCounter;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import java.lang.annotation.ElementType;
 
 @Mod.EventBusSubscriber(modid = "rubidium_extras", bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+
 public class DebugOverlayImprovements
 {
     @SubscribeEvent
+
     public static void onRenderDebugText(RenderGuiOverlayEvent.Pre event) {
-        if (event.getOverlay() != RenderGuiOverlayEvent.ElementType.DEBUG)
+        if (event.getOverlay() != VanillaGuiOverlay.DEBUG_TEXT.type())
             return;
 
         // cancel rendering text if chart is displaying
